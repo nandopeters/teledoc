0bec6588645d76bda3d5ce90125f6b08a3289773
Cosmo Wolfe <templaedhel@gmail.com>
2013-09-07 14:50:59 -0700

    Adding twiml stuff

--- teledoc.py
+++ teledoc.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import twilio.twiml
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming requests."""
+
+    resp = twilio.twiml.Response()
+    resp.say("Hello Monkey")
+
+    return str(resp)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
