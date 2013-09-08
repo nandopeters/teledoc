8867f60c31e66ec4385c5c35deb6f819c78f3835
Martin Steinegger <steinegger_martin@web.de>
2013-09-08 06:21:59 -0700

    roll back

--- symptomelimination.py
+++ symptomelimination.py
@@ -1,6 +1,8 @@
+# import read_country_diseases_prob_file, get_diseases_for_country
+# from symptomProbability import read_symptoms_file, get_symptoms_for_disease, get_symptom_probabilities
+
 import data
 import helpers
-import fileinput
 
 def calculate_probability_for_disease(country, user_symptom_list):
   """Returns diseases based on symptoms the user has"""
@@ -10,7 +12,7 @@ def calculate_probability_for_disease(country, user_symptom_list):
   for disease in diseases_in_country:
     prob = 0
     if len(user_symptom_list) == 0:
-      prob = 0
+      prob = helpers.get_probability_for_disease(disease['disease'],country)
     else:
       for symptom in user_symptom_list:
         if symptom in data.symptoms_for_disease[disease['disease']]:
@@ -21,11 +23,12 @@ def calculate_probability_for_disease(country, user_symptom_list):
     disease["probability"] /= normalizing_term
   return disease_probabilty
 
-def get_ordered_symptom_list(country, user_symptom_list, symptom_blacklist):
+def get_ordered_symptom_list(country, user_symptom_list):
   """Returns symptoms to ask the user about based on existing symptoms and location"""
   diseases = data.disease_prob_for_country[country]
   disease_probabilities = {}
   tmp = calculate_probability_for_disease(country, user_symptom_list)
+  print tmp
   for d in tmp:
     disease_probabilities[d['disease']] = d['probability']
   symptoms = {}
@@ -34,8 +37,6 @@ def get_ordered_symptom_list(country, user_symptom_list, symptom_blacklist):
     for symptom in tmp:
       if symptom in user_symptom_list:
         continue
-      if symptom in symptom_blacklist:
-        continue
       sprob = helpers.get_symptom_probability(symptom)
       sprob *= disease_probabilities[disease['disease']]
       if symptom not in symptoms:
@@ -43,3 +44,9 @@ def get_ordered_symptom_list(country, user_symptom_list, symptom_blacklist):
       else:
         symptoms[symptom] += sprob
   return sorted([{'symptom': z[0], 'prob': z[1]} for z in symptoms.iteritems()], reverse=True, cmp=lambda x,y: cmp(x['prob'], y['prob']))
+
+if __name__ =='__main__':
+  # diseasesproblist = read_country_diseases_prob_file()
+  # symptomlist = read_symptoms_file()
+  # print get_ordered_symptom_list(diseasesproblist, symptomlist, 'chn', ['fever', 'headache', 'malaise'])
+  print get_ordered_symptom_list("EGY",["diarrhea","coma","cough"])
