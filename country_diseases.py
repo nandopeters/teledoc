ac0e468f06353785b64099aae8e1aa8d3a03ae40
Tobias Thiel <tobias011@gmail.com>
2013-09-07 20:01:53 -0700

    finished retrieval of diseases for country

---
+++ country_diseases.py
@@ -0,0 +1,18 @@
+from csvreader import readcsv
+
+def read_diseases_prob_file():
+  return readcsv('data/disease_prob_for_country.csv')
+
+def get_diseases_for_country(country_disease_list, country_code):
+  disease_codes =[]
+  for disease in country_disease_list:
+    if disease['country'] == country_code:
+      print 'got ' + str(disease)
+      disease_codes.append(disease['disease'])
+  return disease_codes
+
+if __name__ == '__main__':
+  data = read_diseases_prob_file()
+  print data
+  print get_diseases_for_country(data, 'deu')
+
