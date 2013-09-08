0d731e68ccfb66bba68eb2d2b85f129cdfd92454
Tobias Thiel <tobias011@gmail.com>
2013-09-07 20:06:38 -0700

    removed debug output

--- country_diseases.py
+++ country_diseases.py
@@ -7,12 +7,10 @@ def get_diseases_for_country(country_disease_list, country_code):
   disease_codes =[]
   for disease in country_disease_list:
     if disease['country'] == country_code:
-      print 'got ' + str(disease)
       disease_codes.append({ 'disease': disease['disease'], 'prob': float(disease['prob']) })
   return disease_codes
 
 if __name__ == '__main__':
   data = read_diseases_prob_file()
-  print data
   print get_diseases_for_country(data, 'deu')
 
